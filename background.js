fe6e8fa5ca6252466203fcf23aca3b3f0e6bd803
jean <you@example.com>
2024-06-11 12:28:39 +0300

    updates on the adblocker

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
-chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: [{
-      "id": 1,
-      "priority": 1,
-      "action": { "type": "block" },
-      "condition": {
-        "urlFilter": "ad",
-        "resourceTypes": ["script", "image", "sub_frame"]
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        "id": 1,
+        "priority": 1,
+        "action": { "type": "block" },
+        "condition": {
+          "urlFilter": "ad",
+          "resourceTypes": ["script", "image", "sub_frame"]
+        }
       }
-    }],
+    ],
     removeRuleIds: [1]
   });
-  
+});
